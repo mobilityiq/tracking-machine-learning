f73fe905d07e98321a0888b42475123c3de95009
Jose Catala <jfca68@gmail.com>
2023-07-25 13:03:37 +0200

    New model and new app.py

--- www/app.py
+++ www/app.py
@@ -1,5 +1,8 @@
 import os
 import tensorflow as tf
+import numpy as np
+from io import StringIO
+from sklearn.preprocessing import LabelEncoder
 from datetime import datetime
 from flask import Flask, request
 
@@ -20,36 +23,79 @@ def predict():
     file = request.files['file']
 
     # Check if the file has an allowed extension
-    if not file.filename.lower().endswith(('.csv')):
-        return 'Authentication failed. Your connection has been recorded and will be reported'
+    # if not filename.lower().endswith(('.csv')):
+    #     return 'Authentication failed. Your connection has been recorded and will be reported'
 
-    # Save the uploaded file to the uploads folder
-    current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"predict_uploaded_file_{current_datetime}.csv"
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(file_path)
+    # Load the new data for prediction
+    new_data = np.genfromtxt(StringIO(file.read().decode('utf-8')), delimiter=',', dtype=float)
 
-    # Call the second script for prediction
-    command = f"python {second_script_path} {file_path}"
-    prediction = os.popen(command).read()
-    print(prediction)
-    
-    # Extract the mode probabilities from the prediction output
-    mode_probabilities_index = prediction.find("Mode Probabilities:")
-    if mode_probabilities_index == -1:
-        return "No mode probabilities found."
+    # Extract the relevant columns from the new data
+    timestamps = new_data[:, 0]
+    speed = new_data[:, 1]
+    x = new_data[:, 2]
+    y = new_data[:, 3]
+    z = new_data[:, 4]
 
-    start_index = mode_probabilities_index + len("Mode Probabilities:") + 1
-    end_index = prediction.find("\n", start_index)
-    mode_probabilities_line = prediction[start_index:end_index]
+    # Load the trained model
+    loaded_model = tf.keras.models.load_model(os.path.join(os.path.dirname(__file__), '..', 'model', 'trained_model-2.0.h5'))
 
-    # Get the first word after "Mode Probabilities:" and remove the colon (:) if present
-    mode = mode_probabilities_line.split()[0].rstrip(":")
+    # Load the label encoder
+    label_encoder = LabelEncoder()
+    label_encoder.classes_ = np.load('../model/label_encoder.npy')
 
-    # Delete the uploaded file after making the prediction
-    os.remove(file_path)
+    # Perform preprocessing on the new data
+    data = np.column_stack((timestamps, speed, x, y, z))
 
-    return mode
+    mean = np.load('../model/mean.npy')
+    std = np.load('../model/std.npy')
+
+    normalized_data = (data - mean) / std
+
+    # Expand dimensions to match the LSTM input shape
+    normalized_data = np.expand_dims(normalized_data, axis=2)
+    normalized_data = np.reshape(normalized_data, (-1, 5, 1))
+
+
+    # Print the intermediate values for debugging
+    # print("Normalized New Data:")
+    # print(normalized_new_data)
+
+    # Make predictions on the new data
+    predictions = loaded_model.predict(normalized_data)
+
+    # Get the predicted labels
+    predicted_labels = label_encoder.inverse_transform(np.argmax(predictions, axis=1))
+
+    # Get the probabilities for each predicted label
+    probabilities = np.max(predictions, axis=1)
+
+    # Create a dictionary to store the mode probabilities
+    mode_probabilities = {}
+
+    # Iterate over the predicted labels and probabilities
+    for label, probability in zip(predicted_labels, probabilities):
+        label = label.upper()
+        if label not in mode_probabilities:
+            mode_probabilities[label] = []
+        mode_probabilities[label].append(probability)
+
+    # Calculate the average probability for each mode
+    average_probabilities = {
+        mode: np.mean(probabilities)
+        for mode, probabilities in mode_probabilities.items()
+    }
+
+    # Sort the mode probabilities in descending order
+    sorted_probabilities = sorted(average_probabilities.items(), key=lambda x: x[1], reverse=True)
+
+    print("Mode Probabilities:")
+    for mode, probability in sorted_probabilities:
+        print(f"{mode}: {probability}")
+
+    probability = sorted_probabilities[0]
+
+    return probability[0]
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
