cd21e4e529836318bb430d0a6de3e7aa7d231e94
Jose Catala <jfca68@gmail.com>
2023-08-07 23:36:32 +0200

    First try with CNN-BiLSTM

--- training/training-4.0.py
+++ training/training-4.0.py
@@ -1,185 +1,162 @@
-import sys
+import os
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
-from tensorflow.keras import regularizers
+import csv
+import time
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from enum import Enum
-import coremltools as ct
-import json
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation
-from tensorflow.keras.utils import to_categorical
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv1D, MaxPooling1D, BatchNormalization, Concatenate, Bidirectional, LSTM, Dense, Dropout, Flatten
+
+def read_motion_data(file_path):
+    return np.genfromtxt(file_path, delimiter=' ', dtype=float, usecols=[0, 1, 2, 3, 10, 11, 12, 13])[::5]
+
+def read_label_file(file_path):
+    return np.genfromtxt(file_path, delimiter=' ', dtype=int, usecols=[1])
+
+def compute_magnitude(d):
+    return np.sqrt(d[0]**2 + d[1]**2 + d[2]**2)
+
+def compute_jerk(data, timestamps):
+    jerk_data = []
+    for i in range(len(data) - 1):
+        delta_t = timestamps[i + 1] - timestamps[i]
+        jerk = (data[i + 1] - data[i]) / delta_t
+        jerk_data.append(jerk)
+    return jerk_data
+
+def create_model(input_shape):
+    # Input
+    inputs = Input(shape=input_shape)
+
+    # CNN Layers
+    conv1 = Conv1D(32, 15, activation='relu', padding='same')(inputs)
+    conv1 = BatchNormalization()(conv1)
+
+    conv2 = Conv1D(64, 10, activation='relu', padding='same')(conv1)
+    conv2 = BatchNormalization()(conv2)
+
+    conv3 = Conv1D(64, 10, activation='relu', padding='same')(conv2)
+    conv3 = BatchNormalization()(conv3)
+
+    conv4 = Conv1D(128, 5, activation='relu', padding='same')(conv3)
+    conv4 = BatchNormalization()(conv4)
+
+    conv5 = Conv1D(128, 5, activation='relu', padding='same')(conv4)
+    conv5 = BatchNormalization()(conv5)
+
+    concatenated = Flatten()(conv5)
+
+    # BiLSTM Layer
+    reshaped = tf.keras.layers.Reshape((1, 384))(concatenated)
+    bi_lstm = Bidirectional(LSTM(128, return_sequences=True))(reshaped)
+    bi_lstm = Flatten()(bi_lstm)
+
+    # Fully Connected Layers
+    fc1 = Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01))(bi_lstm)
+    fc2 = Dense(9, activation='softmax')(fc1)  # 9 classes for transportation modes
+
+    model = Model(inputs, fc2)
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+
+    return model
 
 
-# Define the transportation mode Enum
-class TransportationMode(Enum):
-    DRIVING = 'driving'
-    CYCLING = 'cycling'
-    TRAIN = 'train'
-    BUS = 'bus'
-    SUBWAY = 'metro'
-    STATIONARY = 'stationary'
-    WALKING = 'walking'
-    RUNNING = 'running'
-    UNKNOWN = 'unknown'
-
-# Check if the data file is provided as a command-line argument
-if len(sys.argv) < 2:
-    print("Please provide the path to the data file as a command-line argument.")
-    sys.exit(1)
-
-# Get the data file path from the command-line argument
-data_file = sys.argv[1]
-
-# Load data from the text file
-data = np.genfromtxt(data_file, delimiter=',', dtype=str)
-
-
-# Extract relevant information from the loaded data
-modes = data[:, -1]  # transportation modes
-timestamps = data[:, 0].astype(float)  # timestamps
-x = data[:, 1].astype(float)  # x accel value
-y = data[:, 2].astype(float)  # y accel
-z = data[:, 3].astype(float)  # z accel
-mx = data[:, 4].astype(float)  # mx magnetometer
-my = data[:, 5].astype(float)  # my magnetometer
-mz = data[:, 6].astype(float)  # mz magnetometer
-
-
-# Perform any necessary preprocessing steps
-# For example, you can normalize the sensor values
-
-# Normalize timestamp, speed, x, y, and z values
-def normalize(array):
-    mean = np.mean(array)
-    std = np.std(array)
-    normalized = (array - mean) / std
-    return normalized, mean, std
-
-# Perform normalization on the sensor values
-normalized_timestamp, mean_timestamp, std_timestamp = normalize(timestamps)
-normalized_x, mean_x, std_x = normalize(x)
-normalized_y, mean_y, std_y = normalize(y)
-normalized_z, mean_z, std_z = normalize(z)
-normalized_mx, mean_mx, std_mx = normalize(mx)
-normalized_my, mean_my, std_my = normalize(my)
-normalized_mz, mean_mz, std_mz = normalize(mz)
-
-# Encode transportation modes as numerical labels
-label_encoder = LabelEncoder()
-encoded_labels = label_encoder.fit_transform(modes)
-num_classes = len(TransportationMode)
-
-# Get the list of transportation mode labels
-labels = label_encoder.classes_.tolist()
-
-# Combine normalized sensor values into features
-features = np.column_stack((normalized_timestamp, normalized_x, normalized_y, normalized_z, normalized_mx, normalized_my, normalized_mz))
-
-# Split the data into training and testing sets
-train_features, test_features, train_labels, test_labels = train_test_split(features, encoded_labels, test_size=0.2)
-
-# Define learning rate schedule function
-def lr_schedule(epoch):
-    lr = 1e-3
-    if epoch > 10:
-        lr *= 1e-1
-    elif epoch > 20:
-        lr *= 1e-2
-    print('Learning rate: ', lr)
-    return lr
-
-input_dim = features.shape[1]
-
-
-# Define model with L2 regularization
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(64, input_dim=input_dim, kernel_regularizer=regularizers.l2(0.01)))
-model.add(tf.keras.layers.Activation('relu'))
-model.add(tf.keras.layers.Dense(num_classes))
-model.add(tf.keras.layers.Activation('softmax'))
-
-
-# Define callbacks
-early_stopping = EarlyStopping(patience=3, restore_best_weights=True)
-checkpoint = ModelCheckpoint('model.h5', save_best_only=True)
-lr_scheduler = LearningRateScheduler(lr_schedule)
-
-# Compile and fit the model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
-# One-hot encode the labels
-train_labels = to_categorical(train_labels, num_classes=num_classes)
-test_labels = to_categorical(test_labels, num_classes=num_classes)
-
-history = model.fit(train_features, train_labels, epochs=10, batch_size=32, 
-                    validation_data=(test_features, test_labels),
-                    callbacks=[early_stopping, checkpoint, lr_scheduler])
-
-
-# Save the trained model as a TensorFlow h5 file
-model.save('../model/trained_model-4.0.h5')
-
-# Save the label encoder
-np.save('../model/label_encoder.npy', label_encoder.classes_)
-
-# Save the mean and standard deviation
-np.save('../model/mean.npy', [mean_timestamp, mean_x, mean_y, mean_z, mean_mx, mean_my, mean_mz])
-np.save('../model/std.npy', [std_timestamp, std_x, std_y, std_z, std_mx, std_my, std_mz])
-
-
-# Create a dictionary to hold the metadata
-metadata = {
-    'mean': [mean_timestamp, mean_x, mean_y, mean_z, mean_mx, mean_my, mean_mz],
-    'std': [std_timestamp, std_x, std_y, std_z, std_mx, std_my, std_mz],
-    'labels': ['driving','cycling','train','bus','metro','walking','stationary','running','unknown']
+#  Still=1, Walking=2, Run=3, Bike=4, Car=5, Bus=6, Train=7, Subway=8
+fine_label_mapping = {
+    0: "unknown",
+    1: "stationary",
+    2: "walking",
+    3: "running",
+    4: "cycling",
+    5: "driving",
+    6: "bus",
+    7: "train",
+    8: "metro"
 }
 
-# Convert the metadata dictionary to JSON string
-metadata_json = json.dumps(metadata)
+# users = ["User1", "User2", "User3"]
+users = ["User0"]
+all_data = []
+start_time = time.time()
+motion_files = ["Bag_Motion.txt", "Hips_Motion.txt", "Hand_Motion.txt", "Torso_Motion"]
+# motion_files = ["Bag_Motion.txt"]
 
-# Save the metadata as a JSON file
-with open('../model/metadata.json', 'w') as f:
-    f.write(metadata_json)
+for user in users:
+    data_loading_start = time.time()
+    user_folder = os.path.join("files", user)
+    dates_folders = [folder for folder in os.listdir(user_folder) if not folder.startswith('.')]
 
-# Convert the model to Core ML format with a single input
-# input_shape = (1, features.shape[1])
-# input_feature = ct.TensorType(shape=input_shape)
+    for date_folder in dates_folders:
+        label_file_path = os.path.join(user_folder, date_folder, "Label.txt")
 
-# coreml_model = ct.convert(model, inputs=[input_feature], source='tensorflow')
-coreml_model = ct.convert(model)
+        try:
+            labels = read_label_file(label_file_path)
+        except FileNotFoundError:
+            print(f"Warning: {label_file_path} not found. Skipping...")
+            continue
 
-# Add the metadata to the model as user-defined metadata
-coreml_model.user_defined_metadata['preprocessing_metadata'] = metadata_json
+        for motion_file in motion_files:
+            motion_file_path = os.path.join(user_folder, date_folder, motion_file)
 
-# Set the prediction_type to "probability"
-coreml_model.user_defined_metadata['prediction_type'] = 'probability'
+            if os.path.exists(motion_file_path):
+                data = read_motion_data(motion_file_path)
 
-# Save the Core ML model
-coreml_model.save('../model/TransitModePredictor.mlmodel')
+                timestamps = [row[0] for row in data]
+                acc_data = [row[1:4] for row in data]
+                mag_data = [row[4:7] for row in data]
 
-# Plot training & validation accuracy values
-plt.figure(figsize=(12, 4))
+                acc_magnitudes = [compute_magnitude(d) for d in acc_data]
+                mag_magnitudes = [compute_magnitude(d) for d in mag_data]
+                
+                acc_jerks = compute_jerk(acc_magnitudes, timestamps)
+                mag_jerks = compute_jerk(mag_magnitudes, timestamps)
 
-plt.subplot(1, 2, 1)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
+                # for idx, row in enumerate(data[:-1]):
+                #     if np.isnan(row).any():
+                #         continue
+                    
+                #     # Check for NaN values in the computed jerks
+                #     if np.isnan(acc_jerks[idx]).any() or np.isnan(mag_jerks[idx]).any():
+                #         print("Skipping current row due to NaN values in jerks.")
+                #         continue
 
-# Plot training & validation loss values
-plt.subplot(1, 2, 2)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
+                #     mode = labels[idx]
+                #     if mode != 0:
+                #         all_data.append(list(row) + [acc_jerks[idx], mag_jerks[idx], mode])
 
-plt.show()
+                for idx, timestamp in enumerate(timestamps[:-1]):
+                    # Ensure jerk calculations are not NaN
+                    if not (np.isnan(acc_jerks[idx]) or np.isnan(mag_jerks[idx])):
+                        mode = labels[idx]
+                        if mode != 0:
+                            all_data.append([timestamp, acc_jerks[idx], mag_jerks[idx], mode])
+
+    print(f"Time taken for data loading for {user}: {time.time() - data_loading_start:.2f} seconds")
+
+print(f"Total time taken for all users: {time.time() - start_time:.2f} seconds")
+
+
+
+# Process the all_data list to separate features and labels
+all_data_np = np.array(all_data)
+
+
+X = all_data_np[:, :-1]
+y = all_data_np[:, -1]
+y = tf.keras.utils.to_categorical(y, num_classes=9)  # Convert labels to one-hot encoding
+
+# Reshape the data
+X = X.reshape(X.shape[0], X.shape[1], 1)
+
+# Splitting data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create the CNN-BiLSTM model
+input_shape = (X_train.shape[1], 1)
+model = create_model(input_shape)
+
+# Train the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=32)
+
+# Save the model if needed
+model.save('cnn_bilstm_model.h5')
